bbf5d070fb74a1d9c3b23d9be2eb67419adfda85
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-11-10 12:12:59 +0800

    Add project files.

---
+++ SampleEntityFramework/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleEntityFramework
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int StudentID { get; set; }
+        public School School { get; set; }
+    }
+    public class School
+    {
+        public string Name { get; set; }
+        public int SchoolID { get; set; }
+        public List<Student> StudentList { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Welcome to first EF demo");
+            using (StudentManagementContext stuContext = new StudentManagementContext())
+            {
+                Student student1 = new Student() { Name = "Irfan" };
+                School school1 = new School() { Name = "Singapore International School" };
+                student1.School = school1;
+                stuContext.Schools.Add(school1);
+                stuContext.Students.Add(student1);
+                stuContext.SaveChanges();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SampleEntityFramework/StudentManagementContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace SampleEntityFramework
+{
+    public class StudentManagementContext : DbContext
+    {
+        public StudentManagementContext() : base("Name=SchoolManagementDBConnectionString")
+        {
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<StudentManagementContext>());
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<StudentManagementContext>());
+           // Database.SetInitializer(new DropCreateDatabaseAlways<StudentManagementContext>());
+            //Database.SetInitializer(new );
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<School> Schools { get; set; }
+    }
+}
